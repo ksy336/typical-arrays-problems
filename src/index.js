17ecd28eb5243d2c65c90ffea3213913cfcd1a8f
Kseniya Klimova <ksuoxy773@gmail.com>
2021-02-28 16:33:33 +0400

    feat: add functions to my tasks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min =  function min(array) {
+    if (arguments.length == 0 || array.length == 0) return 0;
+    return Math.min.apply(null, array);
 }
 
-exports.max = function max (array) {
-  return 0;
+
+exports.max = function max(array) {
+
+    if (arguments.length == 0 || array.length == 0) return 0;
+    return Math.max.apply(null, array);
 }
+
 
 exports.avg = function avg (array) {
-  return 0;
+    if (arguments.length == 0 || array.length == 0) return 0;
+    let total = array.reduce(function (a, b) {
+            return (a + b);
+        });
+    let average = total / array.length;
+    return average;
 }
